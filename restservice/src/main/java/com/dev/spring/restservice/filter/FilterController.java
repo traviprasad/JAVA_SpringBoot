3b1f16e08a68b51da0cfb9c05d483dc157f298fa
ravithotakura <raviprasad.thotakura@e.lloydsbanking.com>
2021-12-11 18:26:27 +0000

    feat: spring boot first examples

--- restservice/src/main/java/com/dev/spring/restservice/filter/FilterController.java
+++ restservice/src/main/java/com/dev/spring/restservice/filter/FilterController.java
@@ -1,10 +1,12 @@
 package com.dev.spring.restservice.filter;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilterController {
 
+    @GetMapping(path = "/filter")
     public void staticFilter(){
 
     }
