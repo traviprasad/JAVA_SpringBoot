518596cb0490739c35677dbecb42a0ddee457d83
ravithotakura <raviprasad.thotakura@e.lloydsbanking.com>
2022-01-04 04:39:05 +0000

    feat: added spring cloud files

--- Spring-Cloud-ConfigServer/src/main/java/com/sp/example/SpringCloudConfigServer/SpringCloudConfigServerApplication.java
+++ Spring-Cloud-ConfigServer/src/main/java/com/sp/example/SpringCloudConfigServer/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.sp.example.SpringCloudConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
